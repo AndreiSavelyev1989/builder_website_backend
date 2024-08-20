1719553bb9da6c718d9478623ed10bf7583b3c61
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2024-08-20 16:20:23 +0300

    create endpoint for user registration/loginization

--- src/index.js
+++ src/index.js
@@ -1,22 +1,84 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const imagesRouter = require("./routes/images-router");
-require("dotenv").config();
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10; // Количество раундов соли для bcrypt
 
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(express.json());
 
-app.use(imagesRouter);
+const port = process.env.PORT || 5000;
 
+// Подключение к MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, {})
-  .then(() => {
-    console.log("Successful connection to MongoDB!");
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  .catch((err) => {
-    console.log("Error connecting to MongoDB:", err);
-  });
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
-app.listen(port, () => {
-  console.log("Server started on port: " + port);
+// Определите схему пользователя
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String,
+  lastLogin: Date,
 });
+
+// Создайте модель Mongoose
+const User = mongoose.model("User", userSchema);
+
+// Роут для регистрации нового пользователя
+app.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, saltRounds); // Хэшируем пароль
+
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+      lastLogin: new Date(),
+    });
+    await newUser.save();
+
+    res
+      .status(201)
+      .json({
+        message: "User registered successfully",
+        user: { username, lastLogin: newUser.lastLogin },
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
+  }
+});
+
+// Роут для логина пользователя
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Обновляем время последнего входа
+      user.lastLogin = new Date();
+      await user.save();
+
+      res
+        .status(200)
+        .json({
+          message: "Logged in successfully",
+          user: { username, lastLogin: user.lastLogin },
+        });
+    } else {
+      res.status(401).json({ message: "Invalid username or password" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "Error logging in", error: err.message });
+  }
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
