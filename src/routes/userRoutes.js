57995329e66d7d654efec728111c8babece6e9f7
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2024-08-28 13:24:40 +0300

    update syntax

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const { authenticateToken } = require("../middlewares/authenticateToken.js");
+const { authenticateToken } = require("../middlewares/authenticateToken");
 
 router.put("/update-profile", authenticateToken, userController.updateProfile);
 
